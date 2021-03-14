899ff7b8b1573ac04c64cf44e3c2a8c5daddc8a5
Aayush-2492 <80565621+Aayush-2492@users.noreply.github.com>
2021-03-14 17:03:04 +0530

    Update Assignment1.py

--- Assignment 1/codes/Assignment1.py
+++ Assignment 1/codes/Assignment1.py
@@ -11,24 +11,62 @@ Original file is located at
 # %pip install functions
 
 import math
+from scipy.stats import binom
+import matplotlib.pyplot as plt
+import array as arr
 
 p=0.1
-#p is the probablity of finding a defective bulb
+#p is the probablity of finding a defective bulb or the probability of success
 
 q=0.9
-#q is the probablity of finding a working bulb
+#q is the probablity of finding a working bulb or the probability of failure 
 
-N=5
-#N is the number of defective bulbs
+n=5
+#n is the number of defective bulbs
 
-def nCr(n,r):
-    f = math.factorial
-    return f(n) / f(r) / f(n-r)
+r_values = list(range(n + 1))
+#Contains the possible values of r i.e. possible number of defective bulbs
 
-def Binomial_Distribution(x):
-  P_x =  nCr(N,x)*(p**x)*(q**(N-x))
-  #P_x is the probablity of finding x defective bulbs
+binom_dist = [binom.pmf(r, n, p) for r in r_values ] 
+#Array containing the probability distribution for each value of r
 
-  return P_x
+print("Theoretical probability of finding 0 defective bulbs : ",binom_dist[0])
 
-print("Probability of finding 0 defective bulbs : ",Binomial_Distribution(0))
+print("Theoretical plot for the distribution :")
+plt.bar(r_values, binom_dist) 
+plt.show()
+
+sample_size=100000
+
+binom_sim = binom.rvs(n,p,size=sample_size)
+#Array containing the simulated probability distribution for each value of r 
+
+count=[0,0,0,0,0,0]
+#Creating list for count
+prob=[0,0,0,0,0,0]
+#Creating list for probability
+
+for i in range(sample_size):
+  if binom_sim[i] == 0:
+    count[0]+=1
+  elif binom_sim[i] == 1:
+    count[1]+=1
+  elif binom_sim[i] == 2:
+    count[2]+=1
+  elif binom_sim[i] == 3:
+    count[3]+=1
+  elif binom_sim[i] == 4:
+    count[4]+=1
+  elif binom_sim[i] == 5:
+    count[5]+=1
+
+for i in range(n+1):
+  prob[i]=count[i]/sample_size
+
+print("Simulated probability of finding 0 defective bulbs : ",prob[0])
+
+print("\nSimulated plot for the distribution :")
+plt.bar(r_values, prob) 
+plt.show()
+
+print("Note that the simulated and theoretical values are approximately equal")
