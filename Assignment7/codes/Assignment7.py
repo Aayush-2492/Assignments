f473808002a68cc75eb3d4d3804ab5a3100d9d83
Aayush-2492 <80565621+Aayush-2492@users.noreply.github.com>
2021-04-11 20:59:50 +0530

    Update Assignment7.py

--- Assignment7/codes/Assignment7.py
+++ Assignment7/codes/Assignment7.py
@@ -1,13 +1,22 @@
-import math
-from scipy.stats import binom
-import matplotlib.pyplot as plt
-import array as arr
+from scipy.stats import expon
 import numpy as np
-import random
+import matplotlib.pyplot as plt
+  
+sim_size=10000
+#number of simulations
 
-x = np.linspace(0, 100, 100) 
-fig = plt.figure() 
-plt.plot(x, np.exp(-x)) 
-plt.xlabel("Value of x")
-plt.ylabel("Probability density function of variable X")
-plt.show()
+x_rvs = expon.rvs(loc=0,scale=1,size=sim_size)
+#Random variable X
+
+y_rvs = expon.rvs(loc=0,scale=1,size=sim_size)
+#Random variable Y
+
+sum=0
+
+z_rvs = list(range(sim_size))
+
+for i in range(sim_size):
+  z_rvs[i] = max(x_rvs[i],y_rvs[i])
+  sum=sum+z_rvs[i]
+
+print(sum/sim_size)
